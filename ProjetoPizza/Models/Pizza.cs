36a2be1b0493ddd5c22f8f79f310e118d9cfc642
Thais Barbosa dos Santos <thaisbs1405@gmail.com>
2023-09-03 20:43:14 -0300

    Criando o projeto

---
+++ ProjetoPizza/Models/Pizza.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPizza.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string[] Sabores { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ ProjetoPizza/Program.cs
@@ -0,0 +1,38 @@
+﻿using ProjetoPizza.Models;
+
+Console.WriteLine("Bem-vindo(a) à Pizzaria!\n");
+
+Console.WriteLine("Escolha as opções:");
+Console.WriteLine("1 - Adicionar uma Pizza");
+Console.WriteLine("2 - Listar as Pizzas\n");
+
+Console.Write("Escolha sua opção: ");
+string opcao = Console.ReadLine();
+
+List<Pizza> listaPizzas = new List<Pizza>();
+
+if (opcao == "1") {
+        var pizza = new Pizza();
+
+        Console.Write("Digite o nome da pizza: ");
+        pizza.Nome = Console.ReadLine();
+
+        Console.Write("Digite os sabores da sua pizza (separados por vírgula): ");
+        pizza.Sabores = Console.ReadLine().Split(",");
+
+        Console.Write("Digite o preço da sua pizza (00,00): ");
+        pizza.Preco = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("PIZZA CRIADA COM SUCESSO!");
+        listaPizzas.Add(pizza);
+}
+
+else if (opcao == "2") {
+        foreach (var pizza in listaPizzas)
+        {
+            Console.WriteLine($"Nome: {pizza.Nome}");
+            Console.WriteLine($"Sabores: {string.Join(", ", pizza.Sabores)}"); 
+            Console.WriteLine($"Preço: R$ {pizza.Preco}");
+            Console.WriteLine();
+        }
+}
